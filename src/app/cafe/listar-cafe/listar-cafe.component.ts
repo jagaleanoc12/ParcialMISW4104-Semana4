69bd7383706503d18990c8295cb91754f218c571
jagaleanoc12 <157496758+jagaleanoc12@users.noreply.github.com>
2024-04-27 21:23:38 -0500

    crear componente de listar cafe

---
+++ src/app/cafe/listar-cafe/listar-cafe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-cafe',
+  templateUrl: './listar-cafe.component.html',
+  styleUrls: ['./listar-cafe.component.css']
+})
+export class ListarCafeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
