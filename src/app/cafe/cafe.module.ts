d57bfcebf300b7e74cafa5ebbd6cee8232d5dd18
jagaleanoc12 <157496758+jagaleanoc12@users.noreply.github.com>
2024-04-27 21:42:36 -0500

    incluir HttpClientModule en el modulo cafe

--- src/app/cafe/cafe.module.ts
+++ src/app/cafe/cafe.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarCafeComponent } from './listar-cafe/listar-cafe.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   declarations: [ ListarCafeComponent ],
   exports: [ ListarCafeComponent ]
